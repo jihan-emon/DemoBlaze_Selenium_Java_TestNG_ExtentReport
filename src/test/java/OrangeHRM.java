a81f2c5828041ad81511f6a331436d7b823c48ef
Jihan SQA <emon.esoft@gmail.com>
2021-07-11 00:26:30 +0600

    1st commit

--- src/test/java/OrangeHRM.java
+++ src/test/java/OrangeHRM.java
@@ -1,16 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
+
+@Listeners (ITestListener.class)
 
 public class OrangeHRM {
 
     public static WebDriver driver;
     static  String BaseURL = "https://opensource-demo.orangehrmlive.com/";
+    static JavascriptExecutor js;
 
     @BeforeTest
     public  static void WebSetup() throws InterruptedException {
@@ -23,11 +29,43 @@ public class OrangeHRM {
         Assert.assertTrue(driver.getTitle().contains("OrangeHRM"));
 
 
+    }
+
+    @Test(priority = 0)
+    public static void OrangeHRMLogin() throws Exception {
+
+        MyScreenRecorder.startRecording("");
+
+
+        driver.findElement(By.xpath("//input[@id='txtUsername']")).click();
+        driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys("Admin");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@id='txtPassword']")).click();
+        driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("admin123");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@id='btnLogin']")).click();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        Assert.assertTrue(driver.getPageSource().contains("Dashboard"));
+
+        MyScreenRecorder.stopRecording();
+
+
 
     }
 
-    @Test
-    public static void OrangeHRMLandingPage(){
+    @Test(priority = 1)
+    public static void Dashboard() throws Exception {
+        MyScreenRecorder.startRecording("");
+
+        //driver.findElement(By.xpath("//b[contains(text(),'Dashboard')]")).click();
+        driver.findElement(By.xpath("//b[contains(text(),'Dashboaghjghjgh]")).click();
+        Thread.sleep(1000);
+        ((JavascriptExecutor) driver).executeScript("window.scrollBy(0,450)", "");
+        Thread.sleep(3000);
+        ((JavascriptExecutor) driver).executeScript("window.scrollBy(450,0)", "");
+        Thread.sleep(3000);
+
+        MyScreenRecorder.stopRecording();
 
 
     }
